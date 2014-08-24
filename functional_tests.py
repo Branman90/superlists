0000bf6fe9e7b3d807c600ac8a1c781187d94817
Brandon Potthoff <brandon.t.potthoff@gmail.com>
2014-08-24 13:04:20 -0600

    added story

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,32 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
-browser.get('https://localhost:8000')
+#Edith has heard about a cool new online to-do app. she
+#goes to check out its home page
+browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+#She notices the page title and header mention to-do lists
+assert 'To-Do' in browser.title
+
+#She is invited to enter a to do list right away
+
+#she types "Buy peacock feathers" into a text box (Edith's hobby
+#is tying fly fishing lures)
+
+#When she hits enter, the page updates, and now the page lists 
+#"1 : Buy peacock feathers" as an item in a to-do list
+
+#There is still a text box inviting her to add another item. she 
+#enters "Use peacock feathers to make a fly"
+
+#The page updates again, and now shows both items on her list
+
+#Edith winders whether the site will remember  her list. Then she sees 
+#that the site has generated a unique URL for her -- there is some
+#explanatory text to that effect 
+
+#She visits that URL - her to do list is still there
+
+#Satisfied, she goes back to sleep
+
+browser.quit
